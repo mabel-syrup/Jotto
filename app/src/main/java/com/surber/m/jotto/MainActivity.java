422f4b722df05f7f5765506764e1d513ccb33fe2
mjSurber <matthewsurber96@gmail.com>
2016-11-29 16:15:07 -0600

    Initial commit

---
+++ app/src/main/java/com/surber/m/jotto/MainActivity.java
@@ -0,0 +1,22 @@
+package com.surber.m.jotto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String mySecretWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(mySecretWord.isEmpty()){
+            //No secret word!  Gotta make one.
+        }
+    }
+
+    
+
+
+}
